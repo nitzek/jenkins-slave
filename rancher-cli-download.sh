937cdbb0bc5b77a09cb414f24327142cd41b8572
nitzek <fvorenkamp@gmail.com>
2018-05-15 11:14:42 +0200

    Update rancher-cli-download.sh

--- rancher-cli-download.sh
+++ rancher-cli-download.sh
@@ -3,6 +3,6 @@
 source /.dockerenv
 
 # $github_token set at docker build time via: --build-arg github_token=
-URL=$(curl -H "Authorization: token $github_token" -s https://api.github.com/repos/rancher/cli/releases/latest | jq -r ".assets[] | select(.name | contains(\"linux-amd64\")) | select(.content_type | contains(\"gzip\")) | .browser_download_url")
+URL=$(curl -s https://api.github.com/repos/rancher/cli/releases/latest | jq -r ".assets[] | select(.name | contains(\"linux-amd64\")) | select(.content_type | contains(\"gzip\")) | .browser_download_url")
 
 wget $URL
